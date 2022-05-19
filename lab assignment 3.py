e3af86e3049903180b333a54a636a33597db6927
gitsakshamhub <104558648+gitsakshamhub@users.noreply.github.com>
2022-05-19 14:02:50 +0530

    Add files via upload

---
+++ lab assignment 3.py
@@ -0,0 +1,7 @@
+print("Question 1 ")
+inp = int(input("Enter the number you wish to convert in binary: "))
+b = bin(inp)
+inp1 = b.replace('0b',' ')
+print("The Binary equivalent is :",inp1)
+
+print("Question 2")
